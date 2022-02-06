8f21afc48a6d7539116b8ae7eaa89d69bb9158a5
2022project <ghkdsigm3@gmail.com>
2022-02-06 23:46:04 +0900

    netlify 부분

--- functions/movie.js
+++ functions/movie.js
@@ -1,9 +1,10 @@
 const axios = require('axios')
+const OMDB_API_KEY = process.env.OMDB_API_KEY
 
 exports.handler = async function (event){
     const payload = JSON.parse(event.body)
     const {title, type, year, page, id} = payload    
-    const OMDB_API_KEY = '7035c60c'           
+    //const OMDB_API_KEY = '7035c60c'           
     const url = id 
     ? `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&i=${id}` 
     : `https://www.omdbapi.com/?apikey=${OMDB_API_KEY}&s=${title}&type=${type}&y=${year}&page=${page}`
@@ -17,12 +18,12 @@ exports.handler = async function (event){
             }
         }
         return{
-            statusCod:200,
+            statusCode:200,
             body: JSON.stringify(data)
         }
     } catch(error){
         return {
-            statusCode:error.reponse.status,
+            statusCode:error.response.status,
             body: error.message
         }
     }
